4279737aa19d81823918df9fb246beff234f3aa6
EmersonCamy <143361410+EmersonCamy@users.noreply.github.com>
2024-11-07 09:07:26 -0400

    Delete A2 directory

--- A2/src/main/java/com/a2/repositorio/FornecedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a2.repositorio;
-
-import com.a2.modelo.Fornecedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
-}
--- A2/src/main/java/com/a2/repositorio/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a2.repositorio;
-
-import com.a2.modelo.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-}
