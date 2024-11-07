94d9a5d008bb08eb2148c6c53335801f765247f5
EmersonCamy <143361410+EmersonCamy@users.noreply.github.com>
2024-11-06 23:28:37 -0400

    Delete A2 directory

--- A2/src/main/java/com/a2/repositorio/ItemPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a2.repositorio;
-
-import com.a2.modelo.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
-}
